489174cd6e4fd9c2f9e280b537d9b1e08848cfdb
Alfonso Buononato <74600557+AlfonsoBuononato@users.noreply.github.com>
2021-04-13 16:01:47 +0200

    esercizio

---
+++ js/main.js
@@ -0,0 +1,58 @@
+/* Descrizione
+1. Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+2. Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+3. Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. Una volta creato il nuovo oggetto inserirlo nell'array creato al punto 2 e mostrare alla fine il contenuto dell'array. */
+
+$(document).ready(function(){
+
+    //1. ESERCIZIO
+
+    //CREARE UN OGGETTO ARRAY CONTENENTE LE PROPRIETA NOME COGNOME ETA DELLO STUDENTE 
+
+   /*  var studenti = {
+        nome : "Alfonso",
+        cognome : "Buononato",
+        eta : 24,
+    }; */
+    
+    //STAMPARE ATTRAVERSO UN CICLO FOR LE PROPRIETA DELL OGGETTO
+    /* for(var key in studenti){
+        console.log(key)
+    } */
+
+    //2. ESERCIZIO
+
+    //CREARE UN ARRAY DI OGGETTI.
+    
+/*     var studenti = [
+        {
+            nome : "alfonso",
+            cognome : "buononato",
+            eta : 24,
+        },
+        {
+            nome : "alice",
+            cognome : "aguglia",
+            eta : 25,
+        },
+        {
+            nome : "pasquale",
+            cognome : "vitale",
+            eta : 49,
+        }
+    ]
+
+    for(i = 0; i < studenti.length; i++){
+        var studente = studenti[i];
+        console.log(studente.nome + " " + studente.cognome );
+    } */
+
+
+
+
+
+
+
+
+    //end   
+})
